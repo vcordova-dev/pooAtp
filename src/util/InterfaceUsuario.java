119239fe7082738fbbea248d1789c1665ef43e3c
Vanderlei <vandercordova@yahoo.com.br>
2023-11-23 19:20:40 -0300

    Adicionando a validação de entrada

--- src/util/InterfaceUsuario.java
+++ src/util/InterfaceUsuario.java
@@ -1,53 +1,129 @@
 package util;
 
+import modelo.Apartamento;
+import modelo.Casa;
+import modelo.Financiamento;
+import modelo.Terreno;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InterfaceUsuario {
     //Interface da classe com métodos públicos
-    public static double entradaValorImovel(){
+
+    public static double entradaValorImovel() {
         Scanner scanner = new Scanner(System.in);
-        double valorImovel;
-        do {
-            System.out.print("Digite o valor do imóvel: ");
-            valorImovel = scanner.nextDouble();
-        } while (valorImovel <= 0);
+        double valorImovel = 0;
+
+        while (true) {
+            try {
+                System.out.print("Digite o valor do imóvel: ");
+                valorImovel = scanner.nextDouble();
+
+                if (valorImovel <= 0) {
+                    throw new IllegalArgumentException("O valor do imóvel deve ser maior que zero.");
+                }
+
+                break; // Sair do loop se a entrada for válida
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Digite um número válido.");
+                scanner.nextLine(); // Limpar o buffer do scanner
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
         return valorImovel;
     }
 
-    public static int entradaPrazoFinanciamento(){
+    public static int entradaPrazoFinanciamento() {
         Scanner scanner = new Scanner(System.in);
-        int prazoFinanciamento;
-        do {
-            System.out.print("Digite o prazo do financiamento (em anos): ");
-            prazoFinanciamento = scanner.nextInt();
-        } while (prazoFinanciamento <= 0);
+        int prazoFinanciamento = 0;
+
+        while (true) {
+            try {
+                System.out.print("Digite o prazo do financiamento (em anos): ");
+                prazoFinanciamento = scanner.nextInt();
+
+                if (prazoFinanciamento <= 0) {
+                    throw new IllegalArgumentException("O prazo do financiamento deve ser maior que zero.");
+                }
+
+                break; // Sair do loop se a entrada for válida
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Digite um número válido.");
+                scanner.nextLine(); // Limpar o buffer do scanner
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
         return prazoFinanciamento;
     }
 
-    public static double entradaTaxaJuros(){
+    public static double entradaTaxaJuros() {
         Scanner scanner = new Scanner(System.in);
-        double taxaJurosAnual;
-        do {
-            System.out.print("Digite a taxa de juros anual (em %): ");
-            taxaJurosAnual = scanner.nextDouble();
-            if (taxaJurosAnual > 1000) {
-                System.out.println("A taxa de juros não pode ser maior que 1000%.");
+        double taxaJurosAnual = 0;
+
+        while (true) {
+            try {
+                System.out.print("Digite a taxa de juros anual (em %): ");
+                taxaJurosAnual = scanner.nextDouble();
+
+                if (taxaJurosAnual <= 0 || taxaJurosAnual > 1000) {
+                    throw new IllegalArgumentException("A taxa de juros deve estar entre 0% e 1000%.");
+                }
+
+                break; // Sair do loop se a entrada for válida
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Digite um número válido.");
+                scanner.nextLine(); // Limpar o buffer do scanner
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-        } while (taxaJurosAnual <= 0 || taxaJurosAnual >1000);
+        }
+
         return taxaJurosAnual;
     }
 
     public static int escolherTipoImovel() {
         Scanner scanner = new Scanner(System.in);
-        int escolha;
-        do {
-            System.out.println("Escolha o tipo de imóvel:");
-            System.out.println("1 - Casa");
-            System.out.println("2 - Apartamento");
-            System.out.println("3 - Terreno");
-            System.out.print("Digite o número correspondente ao tipo de imóvel desejado: ");
-            escolha = scanner.nextInt();
-        } while (escolha < 1 || escolha > 3);
+        int escolha = 0;
+
+        while (true) {
+            try {
+                System.out.println("Escolha o tipo de imóvel:");
+                System.out.println("1 - Casa");
+                System.out.println("2 - Apartamento");
+                System.out.println("3 - Terreno");
+                System.out.print("Digite o número correspondente ao tipo de imóvel desejado: ");
+                escolha = scanner.nextInt();
+
+                if (escolha < 1 || escolha > 3) {
+                    throw new IllegalArgumentException("Escolha inválida. Digite um número entre 1 e 3.");
+                }
+
+                break; // Sair do loop se a entrada for válida
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Digite um número válido.");
+                scanner.nextLine(); // Limpar o buffer do scanner
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
         return escolha;
+    }
+
+    public static String getTipoFinanciamento(Financiamento financiamento) {
+        if (financiamento instanceof Casa) {
+            return "Casa";
+        } else if (financiamento instanceof Apartamento) {
+            return "Apartamento";
+        } else if (financiamento instanceof Terreno) {
+            return "Terreno";
+        } else {
+            return "Desconhecido";
+        }
     }
 }
